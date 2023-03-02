6f17e6becffde1b9b5651405ecf6573de1e0a320
Peter Robinson <drinkcoffee@eml.c>
2023-03-02 16:30:47 +1000

    Reworked code - has not been tested

--- contracts/UniswapWormholeMessageReciver.sol
+++ contracts/UniswapWormholeMessageReciver.sol
@@ -22,28 +22,45 @@ interface IWormhole {
 }
 
 contract UniswapWormholeMessageReceiver {
-    string public name = "Uniswap Wormhole Message Receiver";
+    string public name = "Uniswap Wormhole Message ReceiverV2";
 
-    address public owner;
+    // Wormhole chain number for Ethereum
+    uint256 constant ETHERUM = 2;
+
+    // Message timeout in seconds: Time out needs to account for:
+    //  Finality time on source chain.
+    //  Time for Wormhole validators to sign and make VAA available to relayers.
+    //  Time to relay VAA to the target chain.
+    //  Congestion on target chain leading to delayed inclusion of transaction in target chain.
+    // Have the value set to one hour.
+    // Note that there is no way to alter this hard coded value. Including such a feature
+    // would require some governance structure and some minumum and maximum values.
+    uint256 constant MESSAGE_TIME_OUT_SECONDS = 60 * 60;
+
+    // Address of Uniswap Workhole Sender contract on Ethereum.
     bytes32 public messageSender;
 
+    // Mapping (message hash -> bool) indicating a message has already been successfully processed.
     mapping(bytes32 => bool) public processedMessages;
 
+    // Address of Wormhole contract on this chain. Used to parse and verify messages.
     IWormhole private immutable wormhole;
 
-    constructor(address bridgeAddress, bytes32 _messageSender) {
-        wormhole = IWormhole(bridgeAddress);
+    /**
+     * @param _bridgeAddress Address of Wormhole bridge contract on this chain.
+     * @param _messageSender Address of Uniswap Wormhole Message Sender on sending chain.
+     */
+    constructor(address _bridgeAddress, bytes32 _messageSender) {
+        wormhole = IWormhole(_bridgeAddress);
         messageSender = _messageSender;
-        owner = msg.sender;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "sender not owner");
-        _;
-    }
 
-    function receiveMessage(bytes[] memory whMessages) public {
-        (Structs.VM memory vm, bool valid, string memory reason) = wormhole.parseAndVerifyVM(whMessages[0]);
+    /**
+     * @param _whMessage Wormhole message relayed from a remote chain.
+     */
+    function receiveMessage(bytes memory _whMessage) public {
+        (Structs.VM memory vm, bool valid, string memory reason) = wormhole.parseAndVerifyVM(_whMessage);
 
         //validate
         require(valid, reason);
@@ -52,25 +69,26 @@ contract UniswapWormholeMessageReceiver {
         require(messageSender == vm.emitterAddress, "Invalid Emitter Address!");
 
         // Ensure the emitterChainId is Ethereum to prevent impersonation
-        require(2 == vm.emitterChainId , "Invalid Emmiter Chain");
+        require(ETHERUM == vm.emitterChainId , "Invalid Emmiter Chain");
 
-        //verify destination
+        // Verify destination
         (address[] memory targets, uint256[] memory values, bytes[] memory datas, address messageReceiver) = abi.decode(vm.payload,(address[], uint256[], bytes[], address));
         require (messageReceiver == address(this), "Message not for this dest");
 
-        // replay protection
+        // Replay and re-entrancy protection.
         require(!processedMessages[vm.hash], "Message already processed");
         processedMessages[vm.hash] = true;
 
-        //execute message
+        // Don't allow old messages to be processed.
+        require(vm.timestamp + MESSAGE_TIME_OUT_SECONDS < block.timestamp, "Message too old");
+
+        // Check that the target, data, and values arrays are the same length.
         require(targets.length == datas.length && targets.length == values.length, 'Inconsistent argument lengths');
+
+        // Execute all of the function calls in a message.
         for (uint256 i = 0; i < targets.length; i++) {
             (bool success, ) = targets[i].call{value: values[i]}(datas[i]);
             require(success, 'Sub-call failed');
         }
-    }
-
-    function setOwner(address newOwner) external onlyOwner {
-        owner = newOwner;
     }
 }
